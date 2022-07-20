13f35573fcaf72909da8d8ca67b728240e92f2ab
刘子靖 <zijing.liu@geely.com>
2022-07-20 14:56:15 +0800

    init: init project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, reactive } from 'vue'
+import HelloNaruto from './components/HelloNaruto.vue'
+
+const count = ref(0)
+
+const message = reactive({
+  greeting: '❤️ Hello',
+  name: 'Naruto'
+})
+
+const handleAddCount = () => {
+  count.value ++
+}
+
+</script>
+
+<template>
+  <div id="app">
+    <img src="./assets/naruto.jpg" />
+    <HelloNaruto 
+      :message="message" 
+      :count="count" 
+      @onAdd="handleAddCount"
+    />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloNaruto.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+/* v2.7.x内置了defineProps编译器宏，直接在<script setup>内部使用来接收父组件传过来的数据 */
+defineProps({
+  message: { type: Object, default: () => {} },
+  count: { type: Number, default: () => 0 , require: true }
+})
+
+/* v2.7.x内置了defineEmits编译器宏，直接在<script setup>内部使用来发布事件 */
+const emit = defineEmits(['onAdd'])
+const handleAddCount = () => {
+  emit('onAdd')
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ message?.greeting }}  {{ message?.name }}</h1>
+    <button class="count-btn" @click="handleAddCount">{{count}}</button>
+  </div>
+</template>
+
+<style>
+  .count-btn {
+    width: 80px;
+    height: 50px;
+    font-size: 20px;
+  }
+</style>
